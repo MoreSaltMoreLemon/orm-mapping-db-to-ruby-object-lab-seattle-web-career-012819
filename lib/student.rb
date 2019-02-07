81ba5ab61e5acb7cff25f903390b7479eb156eb6
github username <github email address>
2019-02-07 13:48:22 -0800

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,18 +1,70 @@
+require 'pry'
 class Student
   attr_accessor :id, :name, :grade
 
+  # create a new Student object given a row from the database
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    # binding.pry
+    student = Student.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
   end
 
+  # retrieve all the rows from the "Students" database
+  # remember each row should be a new instance of the Student class
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students;
+    SQL
+
+    rows = DB[:conn].execute(sql)
+
+    rows.map do |row|
+      self.new_from_db(row)
+    end
   end
 
+  # find the student in the database given a name
+  # return a new instance of the Student class
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students WHERE students.name = ? LIMIT 1;
+    SQL
+
+    student_row = DB[:conn].execute(sql, name)[0]
+    self.new_from_db(student_row)
+  end
+
+  def self.all_students_in_grade_9
+    self.all.select do |student|
+      student.grade == "9"
+    end
+  end
+
+  def self.students_below_12th_grade
+    self.all.select do |student|
+      student.grade.to_i < 12
+    end
+  end
+
+  def self.first_X_students_in_grade_10(x)
+    puts x
+    students = self.all.select do |student|
+      student.grade == "10"
+    end
+    students[0..(x - 1)]
+  end
+
+  def self.first_student_in_grade_10
+    self.first_X_students_in_grade_10(1).first
+  end
+
+  def self.all_students_in_grade_X(grade)
+    self.all.select do |student|
+      student.grade = grade
+    end
   end
   
   def save
